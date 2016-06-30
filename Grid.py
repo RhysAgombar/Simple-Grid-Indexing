2b3ca90148b6bf43d10ece7f4824aa9a3e6a5507
Rhys Agombar <Rhys.Agombar@uoit.net>
2016-06-29 23:57:34 -0400

    Added Grid with obstruction
    Increasing the length of the lines fixed previous issue

--- Grid.py
+++ Grid.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('grid2.png')
+img = cv2.imread('grido.png')
 
 edges = cv2.Canny(img,50,200,apertureSize = 3)
 
 lineimg = np.zeros([img.shape[0],img.shape[1],img.shape[2]])
 
-lines = cv2.HoughLines(edges,2,np.pi/180,200)
+lines = cv2.HoughLines(edges,2,np.pi/180,300)
 
 mb = np.zeros([lines.shape[0],4])
 
@@ -17,10 +17,10 @@ for i in range (0, lines.shape[0] - 1):
         b = np.sin(theta)
         x0 = a*rho
         y0 = b*rho
-        x1 = int(x0 + 1000*(-b))
-        y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
-        y2 = int(y0 - 1000*(a))
+        x1 = int(x0 + 2000*(-b))
+        y1 = int(y0 + 2000*(a))
+        x2 = int(x0 - 2000*(-b))
+        y2 = int(y0 - 2000*(a))
 
         cv2.line(lineimg,(x1,y1),(x2,y2),(0,0,255),2)
 
